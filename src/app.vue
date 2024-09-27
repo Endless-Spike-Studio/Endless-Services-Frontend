b821fc8da8bf1c9e743aadc8741523382b01a252
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-09-27 20:43:03 +0800

    :sparkles: 新增 `layout`

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,105 @@
-<template>
+<script setup lang="ts">
+import { RouterLink } from '#vue-router'
+import { darkTheme, dateZhCN, lightTheme, type MenuOption, NImage, useOsTheme, zhCN } from 'naive-ui'
+import logo from '~/assets/images/logo.webp'
 
+const osTheme = useOsTheme()
+const theme = computed(() => {
+	switch (osTheme.value) {
+		case 'light':
+			return lightTheme
+		case 'dark':
+			return darkTheme
+		default:
+			return null
+	}
+})
+
+const menus: MenuOption[] = [
+	{
+		label: 'Endless Services',
+		key: '/',
+		icon: () => h(NImage, {
+			src: logo,
+			class: 'rounded',
+			imgProps: {
+				class: 'size-8'
+			}
+		})
+	},
+	{
+		label: 'Endless Proxy',
+		key: '/endless-proxy',
+		children: [
+			{
+				label: 'Newgrounds Proxy',
+				key: '/endless-proxy/newgrounds-proxy',
+				children: [
+					{
+						label: () => h(RouterLink, {
+							to: {
+								path: '/endless-proxy/newgrounds-proxy/songs'
+							}
+						}, {
+							default: () => '歌曲搜索'
+						}),
+						key: '/endless-proxy/newgrounds-proxy/songs'
+					}
+				]
+			}
+		]
+	}
+]
+
+const route = useRoute()
+</script>
+
+<template>
+	<NuxtLayout>
+		<NuxtLoadingIndicator/>
+
+		<n-config-provider :date-locale="dateZhCN" :locale="zhCN" :theme="theme">
+			<n-dialog-provider>
+				<n-loading-bar-provider>
+					<n-message-provider>
+						<n-modal-provider>
+							<n-notification-provider>
+								<n-layout :native-scrollbar="false" position="absolute">
+									<n-layout-header position="absolute">
+										<n-menu :options="menus" mode="horizontal" :value="route.fullPath"/>
+									</n-layout-header>
+
+									<n-layout-content class="container mx-auto">
+										<NuxtPage class="<sm:(px-2)"/>
+									</n-layout-content>
+
+									<n-layout-footer class="p-2" position="absolute">
+										<n-flex :size="0" align="center" vertical>
+											<n-text>&copy; 2024 - {{ new Date().getFullYear() }}</n-text>
+
+											<n-button href='https://beian.miit.gov.cn' tag='a' text>
+												<n-text :depth="3" class="hover:text-current">
+													赣 ICP 备 2022005275 号
+												</n-text>
+											</n-button>
+
+											<n-button href="//www.beian.gov.cn/portal/registerSystemInfo?recordcode=36070202001088" tag="a" text>
+												<n-text :depth="3" class="hover:text-current">
+													赣公网安备 36070202001088 号
+												</n-text>
+											</n-button>
+										</n-flex>
+									</n-layout-footer>
+								</n-layout>
+							</n-notification-provider>
+						</n-modal-provider>
+					</n-message-provider>
+				</n-loading-bar-provider>
+			</n-dialog-provider>
+		</n-config-provider>
+	</NuxtLayout>
 </template>
+
+<style lang="scss">
+@import '~/styles/transitions/page';
+</style>
