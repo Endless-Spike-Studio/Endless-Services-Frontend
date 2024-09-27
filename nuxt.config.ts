4c22ba0862b596edd3a483ecfcd81364f2d826d0
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-09-27 20:24:39 +0800

    :package: :wrench: 安装 `naive-ui` 作为开发依赖, 并配置自动导入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineNuxtConfig({
 	ssr: false,
-	srcDir: 'src'
+	srcDir: 'src',
+	vite: {
+		plugins: [
+			AutoImport({
+				imports: [
+					{
+						'naive-ui': [
+							'useDialog',
+							'useModal',
+							'useMessage',
+							'useNotification',
+							'useLoadingBar'
+						]
+					}
+				]
+			}),
+			Components({
+				resolvers: [NaiveUiResolver()]
+			})
+		]
+	}
 })
