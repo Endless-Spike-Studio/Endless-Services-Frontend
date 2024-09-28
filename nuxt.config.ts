de500099ed26816d4762e676b163964f30edb028
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-09-28 12:43:26 +0800

    :art: 格式化代码

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineNuxtConfig({
 	ssr: false,
