ec74917f2cf1b32683cc8fb7c59612cd5b7019b4
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2025-07-15 21:48:55 +0800

    :heavy_plus_sign: :wrench: 安装并配置 `PrimeVue`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
+import Aura from '@primeuix/themes/aura'
+
+
 export default defineNuxtConfig({
 	compatibilityDate: '2025-05-15',
 	ssr: false,
-	srcDir: 'src'
+	srcDir: 'src',
+	modules: [
+		'@primevue/nuxt-module'
+	],
+	primevue: {
+		options: {
+			theme: {
+				preset: Aura
+			}
+		}
+	}
 })
