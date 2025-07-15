be89712d1e810751dcced4affb94a3a3dacba8a8
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2025-07-15 21:56:44 +0800

    :heavy_plus_sign: :wrench: 安装并配置 `normalize.css`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-05-15',
 	ssr: false,
 	srcDir: 'src',
+	css: [
+		'normalize.css/normalize.css'
+	],
 	modules: [
 		'@unocss/nuxt',
 		'@primevue/nuxt-module'
